03ae9dfdee4f53986d5cfe6ea0a0a1c55ea6a2ce
Ankush Rana <ankush1328.cse18@chitkara.edu.in>
2024-05-21 22:23:01 +0530

    commit 1

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler(IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Business logic to create a product
+
+
+            //create a product entity from command object 
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                Price = command.Price,
+                ImageFile = command.ImageFile,
+            };
+            //ToDo
+            // save in database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+            //return createproductresult
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
